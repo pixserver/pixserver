b1d79193285846f2f97a9d1a54153b8446c8167e
grmask <halves.getters.9t@icloud.com>
2025-04-14 16:59:36 -0400

    Update avbroot and switch to KitsuneMagisk
    Kitsune Mask is not publicly maintained on GitHub anymore. KitsuneMagisk is a fork with the same benefits (Zygisk on Graphene, SuList) but is actively maintained.

--- rooted-ota.sh
+++ rooted-ota.sh
@@ -62,7 +62,7 @@ NO_COLOR=${NO_COLOR:-''}
 OTA_BASE_URL="https://releases.grapheneos.org"
 
 # renovate: datasource=github-releases packageName=chenxiaolong/avbroot versioning=semver
-AVB_ROOT_VERSION=3.14.0
+AVB_ROOT_VERSION=3.15.0
 # renovate: datasource=github-releases packageName=chenxiaolong/Custota versioning=semver-coerced
 CUSTOTA_VERSION=5.7
 # renovate: datasource=git-refs packageName=https://github.com/chenxiaolong/my-avbroot-setup currentValue=master
@@ -227,7 +227,7 @@ function downloadAndroidDependencies() {
 
   mkdir -p .tmp
   if ! ls ".tmp/magisk-$MAGISK_VERSION.apk" >/dev/null 2>&1 && [[ "${POTENTIAL_ASSETS['magisk']+isset}" ]]; then
-    curl --fail -sLo ".tmp/magisk-$MAGISK_VERSION.apk" "https://github.com/HuskyDG/magisk-files/releases/download/1720171347/app-release.apk"
+    curl --fail -sLo ".tmp/magisk-$MAGISK_VERSION.apk" "https://github.com/1q23lyc45/KitsuneMagisk/releases/latest/download/app-release.apk"
   fi
 
   if ! ls ".tmp/$OTA_TARGET.zip" >/dev/null 2>&1; then
